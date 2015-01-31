96980c8d4eeb4298f4eced9a17c80518714edae3
silentdigger <silentdigger@ujm.edu.pl>
2015-01-31 13:33:50 +0100

    3d version

--- admin/index.php
+++ admin/index.php
@@ -9,10 +9,28 @@ if (isset($_SESSION['logged_in'])) {
 } else {
     if (isset($_POST['username'], $_POST['password'])) {
         $username = $_POST['username'];
-        $password = $_POST['password'];
+        $password = md5($_POST['password']);
 
         if (empty ($username) or empty($password)) {
             $error = 'All fields are required!';
+        } else {
+            $query = $pdo->prepare("SELECT * FROM users WHERE user_name = ? AND user_password = ?");
+
+            $query->bindValue(1, $username);
+            $query->bindValue(2, $password);
+
+            $query->execute();
+            $num = $query->rowCount();
+
+            if ($num == 1) {
+                // user entered correct details
+                $_SESSION['logged_in'] = true;
+                header('Location: index.php');
+                exit();
+            } else {
+                // user entered false details
+                $error = 'Incorrect details!';
+            }
         }
 
     }
